5f3626ac5970287dd222c81a72cafb4c69e7dba4
isum450 <leesoomin059@naver.com>
2025-01-11 23:20:46 +0900

    feat : chat_time

--- Main/chat.js
+++ Main/chat.js
@@ -16,7 +16,26 @@ sendButton.addEventListener('click', () => {
 });
 
 socket.on('chatting', (data) => {
-  const li = document.createElement('li');
-  li.innerText = `${data.name} : ${data.msg}`;
-  chatList.appendChild(li);
+  const { name, msg, time } = data;
+  const item = new LiModel(name, msg, time);
+  item.makeLi();
 });
+
+function LiModel(name, msg, time) {
+  this.name = name;
+  this.msg = msg;
+  this.time = time;
+
+  this.makeLi = () => {
+    const li = document.createElement('li');
+    li.classList.add(nickname.value === this.name ? 'sent' : 'received');
+    const dom = `<span class="profile">
+    <span class="user">${this.name}</span>
+    <img class="image" src="https://placeimg.com/50/50/any" alt="any" /></span>
+    <span class="message">${this.msg}</span>
+    <span class="time">${this.time}</span>`;
+
+    li.innerHTML = dom;
+    chatList.appendChild(li);
+  };
+}
--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const http = require('http');
+const moment = require('moment');
 const server = http.createServer(app);
 
 const socketIO = require('socket.io');
@@ -12,30 +13,15 @@ app.use(express.static(path.join(__dirname, 'Main')));
 
 io.on('connection', (socket) => {
     socket.on('chatting', (data) => {
-        const { name, msg, time } = data;
-        const item = new LiModel(name, msg, time);
-        item.makeLi();
-    });
-});
+      const { name, msg } = data;
+      io.emit('chatting', {
+        name,
+        msg,
+        time: moment(new Date()).format('h:mm A'),
+      });
+    }); 
+  });
 
 server.listen(port, '0.0.0.0', () => {
     console.log('Server is running on http://0.0.0.0:3000');
   });
-
-function LiModel(name, msg, time) {
-this.name = name;
-this.msg = msg;
-this.time = time;
-
-this.makeLi = () => {
-    const li = document.createElement('li');
-    li.classList.add(nickname.value === this.name ? 'sent' : 'received');
-    const dom = `<span class="profile">
-    <span class="user">${this.name}</span>
-    <img class="image" src="https://placeimg.com/50/50/any" alt="any" /></span>
-    <span class="message">${this.msg}</span>
-    <span class="time">${this.time}</span>`;
-    li.innerHTML = dom;
-    chatList.appendChild(li);
-};
-}
