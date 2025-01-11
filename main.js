ef0bbe9bbd8313a6eeeb759853c07389937b56bf
isum450 <leesoomin059@naver.com>
2025-01-11 15:31:26 +0900

    style : chat

--- main.js
+++ main.js
@@ -12,10 +12,30 @@ app.use(express.static(path.join(__dirname, 'Main')));
 
 io.on('connection', (socket) => {
     socket.on('chatting', (data) => {
-        io.emit('chatting', data);
+        const { name, msg, time } = data;
+        const item = new LiModel(name, msg, time);
+        item.makeLi();
     });
 });
 
 server.listen(port, '0.0.0.0', () => {
     console.log('Server is running on http://0.0.0.0:3000');
   });
+
+function LiModel(name, msg, time) {
+this.name = name;
+this.msg = msg;
+this.time = time;
+
+this.makeLi = () => {
+    const li = document.createElement('li');
+    li.classList.add(nickname.value === this.name ? 'sent' : 'received');
+    const dom = `<span class="profile">
+    <span class="user">${this.name}</span>
+    <img class="image" src="https://placeimg.com/50/50/any" alt="any" /></span>
+    <span class="message">${this.msg}</span>
+    <span class="time">${this.time}</span>`;
+    li.innerHTML = dom;
+    chatList.appendChild(li);
+};
+}
