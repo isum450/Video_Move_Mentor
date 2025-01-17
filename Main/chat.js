3d6a571e7652a3f6187308010de6fc167016157d
isum450 <leesoomin059@naver.com>
2025-01-17 15:46:37 +0900

    feat : database

--- Main/chat.js
+++ Main/chat.js
@@ -16,6 +16,16 @@ sendButton.addEventListener('click', () => {
   socket.emit('chatting', param);
 });
 
+// 이전 채팅 기록을 처리하는 이벤트
+socket.on('chatHistory', (chatHistory) => {
+  chatHistory.forEach(({ name, msg, time }) => {
+    const item = new LiModel(name, msg, time);
+    item.makeLi();
+  });
+  // 이전 기록이 로드된 후 스크롤을 가장 아래로 이동
+  displayContainer.scrollTo(0, displayContainer.scrollHeight);
+});
+
 socket.on('chatting', (data) => {
   const { name, msg, time } = data;
   const item = new LiModel(name, msg, time);
@@ -54,6 +64,8 @@ function send() {
     msg: chatInput.value,
   };
   socket.emit('chatting', param);
+  chatInput.value = ''; 
 }
 
 sendButton.addEventListener('click', send);
+
--- main.js
+++ main.js
@@ -4,24 +4,49 @@ const port = 3000;
 const path = require('path');
 const http = require('http');
 const moment = require('moment');
-const server = http.createServer(app);
-
+const Database = require("better-sqlite3");
 const socketIO = require('socket.io');
+
+const server = http.createServer(app);
 const io = socketIO(server);
+
+// SQLite 데이터베이스 초기화
+const db = new Database('chat.db');
+
+// 테이블 생성 (없으면 생성)
+db.exec(`
+  CREATE TABLE IF NOT EXISTS chat_logs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    msg TEXT NOT NULL,
+    time TEXT NOT NULL
+  );
+`);
 
 app.use(express.static(path.join(__dirname, 'Main'))); 
 
 io.on('connection', (socket) => {
-    socket.on('chatting', (data) => {
-      const { name, msg } = data;
-      io.emit('chatting', {
-        name,
-        msg,
-        time: moment(new Date()).format('h:mm A'),
-      });
-    }); 
-  });
+  console.log('A user connected');
+
+  const select = db.prepare('SELECT name, msg, time FROM chat_logs ORDER BY id ASC');
+  const chatHistory = select.all();
+  socket.emit('chatHistory', chatHistory);
+
+  socket.on('chatting', (data) => {
+    const { name, msg } = data;
+    const time = moment(new Database()).format('h:mm A');
+
+    const insert = db.prepare('INSERT INTO chat_logs (name, msg, time) VALUES (?, ?, ?)');
+    insert.run(name, msg, time);
+
+    io.emit('chatting', {
+      name,
+      msg,
+      time,
+    });
+  }); 
+});
 
 server.listen(port, '0.0.0.0', () => {
-    console.log('Server is running on http://0.0.0.0:3000');
-  });
+  console.log('Server is running on http://0.0.0.0:3000');
+});
